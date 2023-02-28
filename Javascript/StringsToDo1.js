e031ca2787338f99a29defb2a3fd1be9371f4f06
Brian Denmark <testingarea479@gmail.com>
2023-02-27 16:57:05 -0800

    did it the hardwai to be a good boi

--- Javascript/StringsToDo1.js
+++ Javascript/StringsToDo1.js
@@ -10,10 +10,28 @@
 //     console.log(inputString)
 // }
 
+// EZ PZ
+// function removeWhiteSpace(inputString){
+//     return inputString.replace(/\s/g, ""); 
+// }
+
+// HARD WAI
 function removeWhiteSpace(inputString){
-    return inputString.replace(/\s/g, "");
+    let tempArray = []
+    let tempString = ""
+    for (let i = 0; i < inputString.length; i++){
+        if(inputString.charAt(i)!=" "){
+            tempArray.push(inputString.charAt(i))
+        }
+    }
+    for (let i = 0; i < tempArray.length; i++){
+        tempString += tempArray[i];
+    }
+    return tempString
 }
 
-let myString = "H e llo Der p"
+let myString = "H e llo D               er p"
 
 console.log(removeWhiteSpace(myString));
+
+// Create a JavaScript function that given a string, returns the integer made from the strings digits. You are allowed to use isNaN() and Number().
