5547856907884ed78e95f7aed8c85683002ca79c
Brian Denmark <testingarea479@gmail.com>
2023-03-21 10:37:30 -0700

    Comleted YoureASquare

---
+++ C#/YoureASqure/Program.cs
@@ -0,0 +1,19 @@
+﻿bool IsSquare(int n)
+{
+    bool PerfectSquare = false;
+    if(n % Math.Sqrt(n) == 0 && n > 0 || n == 0)
+    {
+        Console.WriteLine($"{n} is a perfect square.");
+        return PerfectSquare = true;
+    }
+    else
+    {
+        Console.WriteLine($"{n} is NOT a perfect square.");
+        return PerfectSquare;
+    }
+}
+IsSquare(4);
+IsSquare(9);
+IsSquare(10);
+IsSquare(11);
+IsSquare(16);
