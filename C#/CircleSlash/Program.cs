064229cb512e9502042026db8cc8f60c1246317f
Brian Denmark <testingarea479@gmail.com>
2023-04-06 22:21:03 -0700

    code is closer, but not quite there

--- C#/CircleSlash/Program.cs
+++ C#/CircleSlash/Program.cs
@@ -1,54 +1,34 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, CircleSlash!");
+void CheckArray(int[] a)
+{
+    for(int i=0; i < a.Length; i++)
+    {
+        Console.WriteLine(a[i]);
+    }
+}
 
 int CircleSlash(int n)
 {
-    int victorIndex = 0;
-    Boolean noWinner = true;
-    List<int> playerList = new List<int>();
-    for(int i=1; i<=n; i++)
+    int currentPlayer = 1;
+    int victor = 0;
+    bool gameOn = true;
+    int[] defeatedPlayers = new int[n];
+    while(gameOn)
     {
-        playerList.Add(i);
-        // playerList.Insert(i, 0);
-    }
-    while(noWinner)
-    {
-        int i = 0;
-        if(playerList[i+1]!=0)
+        if(defeatedPlayers[currentPlayer-1]==0)
         {
-            Console.WriteLine($"Player {playerList[i]} now has the sword.");
-            Console.WriteLine($"Player {playerList[i+1]} has been eliminated by Player {playerList[i]}.");
-            playerList.RemoveAt(i+1);
-            playerList.Insert(i+1, 0);
-            if(playerList[i+=2]!=0 && i<=playerList.Count)
-            {
-                i+=2;
-                Console.WriteLine($"Player {playerList[i]} now has the sword.");
-            }
-            else if(playerList[i+=2]!=0 && i>=playerList.Count)
-            {
-                i=0;
-                if(playerList[i]!=0)
-                {
-                    Console.WriteLine($"Player {playerList[i]} now has the sword.");
-                    i+=2;
-                }
-                else if(playerList[i+1]!=0)
-                {
-                    Console.WriteLine($"Player {playerList[i]} now has the sword.");
-                    i+=2;
-                }
-            }
-        }
-        else if(playerList[i+1]==0)
-        {
-            i++;
-            continue;
+            CheckArray(defeatedPlayers);
+            Console.WriteLine($"Current player: {currentPlayer} | Status: {defeatedPlayers[currentPlayer-1]}");
+            Console.WriteLine($"{currentPlayer} kills {currentPlayer + 1} and passes to {currentPlayer + 2}");
+            Console.WriteLine($"Player {currentPlayer + 1} status set to: {defeatedPlayers[currentPlayer + 1] = 1}");
+            currentPlayer++;
+            CheckArray(defeatedPlayers);
+            Console.WriteLine($"Next player: {currentPlayer}");
         }
     }
-
-    Console.WriteLine(playerList.Count);
-    return victorIndex;
+    return victor;
 }
+
 
 CircleSlash(5);
