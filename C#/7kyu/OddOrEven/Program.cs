0ba5b0bb77b555ba27d48002d9e53c8f18294127
Brian Denmark <testingarea479@gmail.com>
2023-04-09 21:25:03 -0700

    Solved Odd or Even

--- C#/7kyu/OddOrEven/Program.cs
+++ C#/7kyu/OddOrEven/Program.cs
@@ -1,2 +1,21 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+Console.WriteLine("Hello, Odd or Even!");
+
+string OddOrEven(int[] array)
+{
+    int sum = 0;
+    for(int i=0; i<array.Length; i++)
+    {
+        sum+= array[i];
+    }
+    if(sum%2==0 || sum==0)
+    {
+        return "even";
+    }
+    else
+    {
+        return "odd";
+    }
+}
+
+Console.WriteLine(OddOrEven(new int[]{1,3,5}));
