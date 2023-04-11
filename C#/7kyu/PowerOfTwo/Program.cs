4551a0fb91dcf5be6aabc777c68afbcf101d6088
Brian Denmark <testingarea479@gmail.com>
2023-04-10 20:08:25 -0700

    Solved it, badly, but at least it works!

---
+++ C#/7kyu/PowerOfTwo/Program.cs
@@ -0,0 +1,26 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, PowerOfTwo!");
+
+// This is a troll method, but it is kinda a meme xD
+bool PowerOfTwo(int n)
+{
+    bool IsPowerOfTwo = false;
+    int Product = 2;
+    do
+    {
+        Product = Product * 2;
+    } while (Product < n);
+
+    if(Product == n || n == 1 || n == 2)
+    {
+        Console.WriteLine($"Product: {Product} | n: {n}");
+        return IsPowerOfTwo = true;
+    }
+    else
+    {
+        Console.WriteLine($"Product: {Product} | n: {n}");
+        return IsPowerOfTwo;
+    }
+}
+
+Console.WriteLine(PowerOfTwo(2));
