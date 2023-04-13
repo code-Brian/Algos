0942fa13c3a1bd140b2c584385dfa64138ccdd63
Brian Denmark <testingarea479@gmail.com>
2023-04-13 05:44:54 -0700

    solved algo

---
+++ C#/7kyu/OddEvenStringSort/Program.cs
@@ -0,0 +1,28 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+string SortMyString(string s)
+{
+    // Take the incoming string iterate over each character.
+    string SortedString="";
+    string EvenString="";
+    string OddString="";
+    // Iterate over the input string
+    for(int i=0; i<s.Length; i++)
+    {
+        // Sort it into even and odd strings
+        if(i == 0 || i % 2 == 0)
+        {
+            EvenString+=s[i];
+        }
+        else
+        {
+            OddString+=s[i];
+        }
+    }
+    // Concat the strings at the end
+    SortedString = EvenString + " " + OddString;
+    Console.WriteLine(SortedString);
+    return SortedString;
+}
+
+SortMyString("CodeWars");
