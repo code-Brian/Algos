a5723331946e019c0255ad53d95ea31650eac35e
Brian Denmark <testingarea479@gmail.com>
2023-04-30 22:57:42 -0700

    complete insertion into List<string> from string

--- C#/7kyu/SortByLastChar/Program.cs
+++ C#/7kyu/SortByLastChar/Program.cs
@@ -10,13 +10,13 @@ string[] Last(string x)
     {
         if(x[i] == char.Parse(" "))
         {
-            Console.WriteLine($"Whitespace detected at index: {i}");
+            // Console.WriteLine($"Whitespace detected at index: {i}");
             SortedByLast.Add(TempString);
             TempString="";
         }
         else if(i == x.Length-1)
         {
-            Console.WriteLine("End of list detected");
+            // Console.WriteLine("End of list detected");
             TempString = TempString + x[i];
             SortedByLast.Add(TempString);
         }
@@ -24,7 +24,27 @@ string[] Last(string x)
         {
             TempString = TempString + x[i];
         }
-        
+        int max = 0;
+        string tempString = "";
+        for(int j=0; j<SortedByLast.Count; j++)
+        {
+            Console.WriteLine($"Current Value of j: {j}");
+            Console.WriteLine($"Last Char: {SortedByLast[j].Last<char>()}");
+            Console.WriteLine($"Last Char to ascii: {(int)SortedByLast[j].Last<char>()}");
+            if(j==0)
+            {
+                max = (int)SortedByLast[j].Last<char>();
+                Console.WriteLine($"Initial Max set to: {max}");
+            }
+            else if((int)SortedByLast[j].Last<char>() < max)
+            {
+                max = (int)SortedByLast[j].Last<char>();
+                tempString = SortedByLast[0];
+                SortedByLast.Insert(0, SortedByLast[j]);
+                SortedByLast.RemoveAt(j);
+                SortedByLast.Insert(j, tempString);
+            }
+        }
     }
     return SortedByLast.ToArray();
 }
