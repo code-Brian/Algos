fe6a8e2a91017e751541ebdc4955185d2f98d0f8
Brian Denmark <testingarea479@gmail.com>
2023-05-16 22:23:30 -0700

    init commit

---
+++ C#/7kyu/SuzukiStudents/Program.cs
@@ -0,0 +1,18 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, Suzuki Frens!");
+
+string[] LineupStudents(String str)
+{
+    string[] Students = str.Split(new[] {' '});
+
+    for (int i=0; i < Students.Length; i++)
+    {
+        Console.WriteLine(Students[i]);
+    }
+
+    return Students;
+}
+
+string TestString = "xxa xxb xxc xxd xa xb xc xd";
+
+LineupStudents(TestString);
