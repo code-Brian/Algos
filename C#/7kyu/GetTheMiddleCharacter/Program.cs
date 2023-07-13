6f45ed0e29e47bb38f75ec98d15b50dc831576ae
Brian Denmark <testingarea479@gmail.com>
2023-07-12 19:25:39 -0700

    solution implemented

---
+++ C#/7kyu/GetTheMiddleCharacter/Program.cs
@@ -0,0 +1,21 @@
+﻿// See https://aka.ms/new-console-template for more information
+string GetMiddle(string str)
+{
+    if(str.Length % 2 == 0)
+    {
+        string middleOfString = str.Substring((str.Length/2) - 1, 2);
+        Console.WriteLine(middleOfString);
+        return middleOfString;
+    }
+    else
+    {
+        int middleIndex = str.Length / 2;
+        string middleOfString = str.Substring(middleIndex,1);
+        Console.WriteLine(middleOfString);
+        return middleOfString;
+    }
+
+}
+
+GetMiddle("but");
+GetMiddle("Asol");
