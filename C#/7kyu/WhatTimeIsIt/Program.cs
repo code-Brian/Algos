3f4a02216fe69b9cb7f9a07cbcf5ef2d492c309f
Brian Denmark <testingarea479@gmail.com>
2023-07-15 13:49:16 -0700

    identifying times that need parsing/converting

---
+++ C#/7kyu/WhatTimeIsIt/Program.cs
@@ -0,0 +1,39 @@
+﻿// See https://aka.ms/new-console-template for more information
+string GetMilitaryTimeFromStandardTime(string time)
+{
+    string convertedTime = "";
+    string tempString = "";
+    bool isEvening = false;
+    bool isMorning = false;
+
+    for(int i = 0; i <time.Length; i++)
+    {
+
+        tempString += time[i];
+
+        if(time[i] == 'A')
+        {
+            isMorning = true;
+            Console.WriteLine("Morning time found.");
+        }
+        else if(time[i] == 'P')
+        {
+            isEvening = true;
+            Console.WriteLine("Evening time found.");
+        }
+        Console.WriteLine(time[i]);
+    }
+
+    if(isMorning == true)
+    {
+        if(time[0] == '1' && time[1] == '2')
+        {
+            Console.WriteLine("Parse needed for AM time containing 12.");
+        }
+    }
+    Console.WriteLine($"isMorning: {isMorning}");
+    Console.WriteLine(tempString);
+    return convertedTime;
+}
+
+GetMilitaryTimeFromStandardTime("12:00:01AM"); // This should return "00:00:01"
