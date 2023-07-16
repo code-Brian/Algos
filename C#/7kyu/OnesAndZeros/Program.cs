76024310be7b8fd52ad9617168f2bedda5f92a89
Brian Denmark <testingarea479@gmail.com>
2023-07-16 13:35:24 -0700

    solved algo

---
+++ C#/7kyu/OnesAndZeros/Program.cs
@@ -0,0 +1,15 @@
+﻿// See https://aka.ms/new-console-template for more information
+int BinaryArrayToNumber(int[] BinaryArray)
+{
+    string binaryString = "";
+    int binaryNumber = 0;
+    for(int i = 0; i<BinaryArray.Length; i++)
+    {
+        binaryString += BinaryArray[i].ToString();
+    }
+    return binaryNumber = Convert.ToInt32(binaryString,2);
+}
+
+int [] binaryTest = new int [] {0,0,0,1};
+
+Console.WriteLine(BinaryArrayToNumber(binaryTest));
