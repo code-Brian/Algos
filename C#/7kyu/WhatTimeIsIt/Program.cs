af9a4e14cf6a535a9f2ec7a8af9a490acdd31154
Brian Denmark <testingarea479@gmail.com>
2023-07-15 23:19:58 -0700

    completed algo

--- C#/7kyu/WhatTimeIsIt/Program.cs
+++ C#/7kyu/WhatTimeIsIt/Program.cs
@@ -3,8 +3,9 @@ string GetMilitaryTimeFromStandardTime(string time)
 {
     string convertedTime = "";
     string tempString = "";
-    bool isEvening = false;
-    bool isMorning = false;
+    int tempInt = 0;
+    bool isPM = false;
+    bool isAM = false;
 
     for(int i = 0; i <time.Length; i++)
     {
@@ -13,27 +14,51 @@ string GetMilitaryTimeFromStandardTime(string time)
 
         if(time[i] == 'A')
         {
-            isMorning = true;
+            isAM = true;
             Console.WriteLine("Morning time found.");
         }
         else if(time[i] == 'P')
         {
-            isEvening = true;
+            isPM = true;
             Console.WriteLine("Evening time found.");
         }
         Console.WriteLine(time[i]);
     }
 
-    if(isMorning == true)
+    if(isAM == true)
     {
-        if(time[0] == '1' && time[1] == '2')
+        if(time.Substring(0,2) == "12")
         {
             Console.WriteLine("Parse needed for AM time containing 12.");
+            convertedTime = "00" + time.Substring(2,6);
+        }
+        else 
+        {
+            convertedTime = time.Substring(0,8);
         }
     }
-    Console.WriteLine($"isMorning: {isMorning}");
-    Console.WriteLine(tempString);
+
+    if(isPM == true)
+    {
+        if(time.Substring(0,2) == "12")
+        {
+            convertedTime = time.Substring(0,8);
+            Console.WriteLine($"convertedTime is: {convertedTime}");
+            return convertedTime = time.Substring(0,8);
+        }
+
+        Console.WriteLine("Evening time found");
+        tempString = time.Substring(0,2);
+        tempInt = int.Parse(tempString) + 12;
+        tempString = tempInt.ToString();
+        convertedTime = tempString + time.Substring(2,6);
+        Console.WriteLine(tempInt);
+    }
+    Console.WriteLine($"convertedTime is: {convertedTime}");
     return convertedTime;
 }
 
-GetMilitaryTimeFromStandardTime("12:00:01AM"); // This should return "00:00:01"
+// GetMilitaryTimeFromStandardTime("12:00:01AM"); // This should return "00:00:01"
+// GetMilitaryTimeFromStandardTime("07:00:00AM");
+GetMilitaryTimeFromStandardTime("07:00:00PM");
+GetMilitaryTimeFromStandardTime("12:00:00PM");
